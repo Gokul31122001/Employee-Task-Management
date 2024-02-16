ca4fdd900f21ef053b804432a6c61ba8bc5e1b9c
Gokul31122001 <88575956+Gokul31122001@users.noreply.github.com>
2024-02-15 22:31:59 -0800

    Add files via upload

---
+++ include.php
@@ -0,0 +1,4 @@
+<IfModule mod_php.c>
+    AddType application/x-httpd-php http://localhost/Employee-taskms-PHP/index.php
+    php_value include_path "C:\xampp\htdocs\Employee-taskms-PHP"
+</IfModule>
